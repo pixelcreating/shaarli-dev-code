7f1bd27b880b722b33248f643eef6cf3d589422d
Qwerty <champlywood@free.fr>
2016-06-13 19:03:19 +0200

    Update self-dead-link.php

--- self-dead-link.php
+++ self-dead-link.php
@@ -5,11 +5,13 @@ if(ob_get_level() == 0) ob_start();
 $begin = microtime(TRUE);
 function get_headers_curl($url) {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,            $url);
-    curl_setopt($ch, CURLOPT_HEADER,         true);
-    curl_setopt($ch, CURLOPT_NOBODY,         true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT,        15);
+    curl_setopt($ch, CURLOPT_URL,           	$url);
+    curl_setopt($ch, CURLOPT_HEADER,        	true);
+    curl_setopt($ch, CURLOPT_NOBODY,			true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,	true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION,	true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,	false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,	false);
 
     $r = curl_exec($ch);
 	return empty(substr($r, 9, 3)) ? 0 : substr($r, 9, 3);
@@ -26,7 +28,7 @@ $double_website=(fopen('double_website.csv','a+'));
 
 fwrite($down_website,'"http_status", "url", "title"'.PHP_EOL);
 
-$datastore = unserialize(gzinflate(base64_decode(substr(file_get_contents('data/datastore.php'),strlen('<?php /* '),-strlen(' */ ?>')))));
+$datastore = unserialize(gzinflate(base64_decode(substr(file_get_contents('datastore.php'),strlen('<?php /* '),-strlen(' */ ?>')))));
 foreach($datastore as $shaarlink) {
 	if(substr($shaarlink['url'], 0, 1) != '?') { # on vérifie que c’est pas un lien local
 		$link[] = $shaarlink['url'];
