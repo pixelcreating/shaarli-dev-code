577e5c546c832213b89b316c8a0e154cb6480f35
Qwerty <champlywood@free.fr>
2016-06-13 18:27:11 +0200

    Update self-dead-link.php

--- self-dead-link.php
+++ self-dead-link.php
@@ -3,51 +3,39 @@ error_reporting(0);
 set_time_limit(0);
 if(ob_get_level() == 0) ob_start();
 $begin = microtime(TRUE);
-function get_http_response_code($theURL) { # http://fr2.php.net/manual/fr/function.get-headers.php#97684
-   $headers = @get_headers($theURL);
-   if(empty($headers[0])) {
-	   $code = 000;
-   }
-   else {
-	   $code = substr($headers[0], 9, 3);
-   }
-   return $code;
+function get_headers_curl($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,            $url);
+    curl_setopt($ch, CURLOPT_HEADER,         true);
+    curl_setopt($ch, CURLOPT_NOBODY,         true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT,        15);
+
+    $r = curl_exec($ch);
+	return empty(substr($r, 9, 3)) ? 0 : substr($r, 9, 3);
+	return $r;
 }
 function get_title($url) {
 	preg_match('#<title ?[^>]*>(.*)</title>#Usi', file_get_contents($url), $title);
 	return isset($title[1]) ? $title[1] : null;
 }
 $link = array();
-$title = array();
-$datastore = unserialize(gzinflate(base64_decode(substr(file_get_contents('data/datastore.php'),strlen('<?php /* '),-strlen(' */ ?>')))));
-$fileopen=(fopen('website.csv','a+'));
-$total = count($datastore);
-$progress = 0;
-fwrite($fileopen,'"http_status", "url", "different_title", "old_title", "new_title"'.PHP_EOL);
+
+$down_website=(fopen('down_website.csv','a+'));
+$double_website=(fopen('double_website.csv','a+'));
+
+fwrite($down_website,'"http_status", "url", "title"'.PHP_EOL);
+
+$datastore = unserialize(gzinflate(base64_decode(substr(file_get_contents('datastore.php'),strlen('<?php /* '),-strlen(' */ ?>')))));
 foreach($datastore as $shaarlink) {
-	$link[] = $shaarelink['url'];
-	$progress +=1;
-	$statut = get_http_response_code($shaarlink['url']);
-	if($statut != 200) {
-		$title[] = $shaarelink['title'];
-		$newtitle = get_title($shaarlink['url']);
-		$status_title = ($shaarelink['title'] != $newtitle) ? 1 : 0;
-		$msglog = '"'.$statut.'", "'.$shaarlink['url'].'",'.'"'.$status_title.'", "'.$shaarelink['title'].'", "'.$newtitle.'"'.PHP_EOL;
-		echo floor($progress/$total).'% ('.$progress.'/'.$total.') '.$statut.' '.$shaarlink['url'].PHP_EOL;
-		ob_flush();
-        flush();
-		fwrite($fileopen,$msglog);
+	if(substr($shaarlink['url'], 0, 1) != '?') { # on vérifie que c’est pas un lien local
+		$link[] = $shaarlink['url'];
+		$statut = get_headers_curl($shaarlink['url']);
+		fwrite($down_website,'"'.$statut.'", "'.$shaarlink['url'].'", "'.get_title($shaarlink['url']).'"'.PHP_EOL);
 	}
 }
 foreach(array_count_values($link) as $k=>$v) {
 	if($v >1) {
-		$msglog = '"DOUBLON", "'.$k.'"'.PHP_EOL;
-	}
-	fwrite($fileopen,$msglog);
+		fwrite($double_website,'"DOUBLON", "'.$k.'"'.PHP_EOL);
+	}	
 }
-$end = microtime(TRUE);
-echo round(($end - $begin),6).' seconds';
-echo "Done";
-fclose($fileopen);
-ob_end_flush();
-?>
