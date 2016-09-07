d8470789e73b313ebcc3eee351eff53c8b043ce0
Qwerty <champlywood@free.fr>
2016-09-07 09:14:02 +0200

    Update self-dead-link.php

--- self-dead-link.php
+++ self-dead-link.php
@@ -3,41 +3,35 @@ error_reporting(0);
 set_time_limit(0);
 if(ob_get_level() == 0) ob_start();
 $begin = microtime(TRUE);
-function get_headers_curl($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,           	$url);
-    curl_setopt($ch, CURLOPT_HEADER,        	true);
-    curl_setopt($ch, CURLOPT_NOBODY,			true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,	true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION,	true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,	false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,	false);
-
-    $r = curl_exec($ch);
-	return empty(substr($r, 9, 3)) ? 0 : substr($r, 9, 3);
-	return $r;
+function get_http_response_code($theURL) { # http://fr2.php.net/manual/fr/function.get-headers.php#97684
+   $headers = @get_headers($theURL);
+   if(empty($headers[0])) {
+	   $code = 000;
+   }
+   else {
+	   $code = substr($headers[0], 9, 3);
+   }
+   return $code;
 }
-function get_title($url) {
-	preg_match('#<title ?[^>]*>(.*)</title>#Usi', file_get_contents($url), $title);
-	return isset($title[1]) ? $title[1] : null;
-}
-$link = array();
-
-$down_website=(fopen('down_website.csv','a+'));
-$double_website=(fopen('double_website.csv','a+'));
-
-fwrite($down_website,'"http_status", "url", "title"'.PHP_EOL);
-
-$datastore = unserialize(gzinflate(base64_decode(substr(file_get_contents('datastore.php'),strlen('<?php /* '),-strlen(' */ ?>')))));
+$datastore = unserialize(gzinflate(base64_decode(substr(file_get_contents('data/datastore.php'),strlen('<?php /* '),-strlen(' */ ?>')))));
+$fileopen=(fopen('website.log','a+'));
+echo '<pre>';
 foreach($datastore as $shaarlink) {
-	if(substr($shaarlink['url'], 0, 1) != '?') { # on vérifie que c’est pas un lien local
-		$link[] = $shaarlink['url'];
-		$statut = get_headers_curl($shaarlink['url']);
-		fwrite($down_website,'"'.$statut.'", "'.$shaarlink['url'].'", "'.get_title($shaarlink['url']).'"'.PHP_EOL);
+	$statut = get_http_response_code($shaarlink['url']);
+	if(!in_array($statut, array(200, 201, 202, 203, 204, 203, 204, 205, 206, 207, 2010, 226, 302, 307))) {
+		$msglog = '('.$statut.') '.$shaarlink['url'].PHP_EOL;
+		$msg = '(<a href="https://en.wikipedia.org/wiki/HTTP_'.$statut.'">'.$statut.'</a>) '.$shaarlink['url'].PHP_EOL;	
+		file_put_contents($statut.'.log', file_get_contents($statut.'.log').PHP_EOL.$shaarlink['url'].PHP_EOL);
+		echo $msg;
+		fwrite($fileopen,$msglog);
+		ob_flush();
+        flush();
 	}
 }
-foreach(array_count_values($link) as $k=>$v) {
-	if($v >1) {
-		fwrite($double_website,'"DOUBLON", "'.$k.'"'.PHP_EOL);
-	}	
-}
+echo '</pre>';
+$end = microtime(TRUE);
+echo round(($end - $begin),6).' seconds';
+echo "Done";
+fclose($fileopen);
+ob_end_flush();
+?>
